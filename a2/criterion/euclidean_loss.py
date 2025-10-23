25f1007d0634873235a5fc94be912e09ed42785b
allanli608 <allanli608@gmail.com>
2025-10-23 15:19:47 +0800

    formatting

--- a2/criterion/euclidean_loss.py
+++ a2/criterion/euclidean_loss.py
@@ -1,33 +1,40 @@
 import numpy as np
 
+
 class EuclideanLossLayer():
-	def __init__(self):
-		self.accu = 0.
-		self.loss = 0.
+    def __init__(self):
+        self.accu = 0.
+        self.loss = 0.
 
-	def forward(self, logit, gt):
-		"""
-	      Inputs: (minibatch)
-	      - logit: forward results from the last FCLayer, shape(batch_size, 10)
-	      - gt: the ground truth label, shape(batch_size, 10)
-	    """
+    def forward(self, logit, gt):
+        """
+      Inputs: (minibatch)
+      - logit: forward results from the last FCLayer, shape(batch_size, 10)
+      - gt: the ground truth label, shape(batch_size, 10)
+    """
 
-		############################################################################
-	    # TODO: Put your code here
-		# Calculate the average accuracy and loss over the minibatch, and
-		# store in self.accu and self.loss respectively.
-		# Only return the self.loss, self.accu will be used in solver.py.
+        ############################################################################
+    # TODO: Put your code here
+        # Calculate the average accuracy and loss over the minibatch, and
+        # store in self.accu and self.loss respectively.
+        # Only return the self.loss, self.accu will be used in solver.py.
+        batch_size = logit.shape[0]
+        self.loss = (np.sum((logit - gt) ** 2) / 2) / batch_size
 
+        prediction = np.argmax(logit, axis=1)
+        truth = np.argmax(gt, axis=1)
 
-	    ############################################################################
+        self.accu = np.sum(prediction == truth) / batch_size
+    ############################################################################
 
-		return self.loss
+        return self.loss
 
-	def backward(self):
+    def backward(self):
 
-		############################################################################
-	    # TODO: Put your code here
-		# Calculate and return the gradient (have the same shape as logit)
-
-
-	    ############################################################################
+        ############################################################################
+        # TODO: Put your code here
+        # Calculate and return the gradient (have the same shape as logit)
+        batch_size = self.accu.shape[0]
+        grad = (self.logit - self.gt) / batch_size
+        return grad
+        ############################################################################
--- a2/layers/sigmoid_layer.py
+++ a2/layers/sigmoid_layer.py
@@ -1,26 +1,25 @@
 import numpy as np
 
+
 class SigmoidLayer():
-	def __init__(self):
-		"""
-		Applies the element-wise function: f(x) = 1/(1+exp(-x))
-		"""
-		self.trainable = False
+    def __init__(self):
+        """
+        Applies the element-wise function: f(x) = 1/(1+exp(-x))
+        """
+        self.trainable = False
 
-	def forward(self, Input):
+    def forward(self, Input):
 
-		############################################################################
-	    # TODO: Put your code here
-		# Apply Sigmoid activation function to Input, and return results.
+        ############################################################################
+        # TODO: Put your code here
+        # Apply Sigmoid activation function to Input, and return results.
 
+        ############################################################################
 
-	    ############################################################################
+    def backward(self, delta):
 
-	def backward(self, delta):
+        ############################################################################
+        # TODO: Put your code here
+        # Calculate the gradient using the later layer's gradient: delta
 
-		############################################################################
-	    # TODO: Put your code here
-		# Calculate the gradient using the later layer's gradient: delta
-
-
-	    ############################################################################
+        ############################################################################
